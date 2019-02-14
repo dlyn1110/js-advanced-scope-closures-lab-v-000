19643e36295c272c7bc5e853bef1e47d67ccfaaa
Dani Lyn <dlyn1110@gmail.com>
2019-02-14 03:31:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function produceDrivingRange(blockRange) {
+  return function (start, end) {
+  let distance = parseInt(end) - parseInt(start)
+
+  if (blockRange > distance){
+    return `within range by ${blockRange - distance}`
+  }
+  else {
+    return `${distance - blockRange} blocks out of range`
+    }
+  }
+
+  }
+
+  function produceTipCalculator(percentage){
+    return function (totalFare){
+      return percentage * totalFare
+    }
+  }
+
+   function createDriver(){
+     let driverId = 0;
+
+     return class {
+       constructor(name){
+         this.name = name;
+         this.id = ++driverId;
+       }
+     }
+   }
